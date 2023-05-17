e86d380dd6eccf9b7622fbb533c7cce163eea9e9
Ifthikar Ali <ifthicaralikhan@gmail.com>
2023-05-17 18:33:25 -0500

    added a div tag to the tale

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,20 +1,24 @@
 <template>
-<table>
-  <tr>
-    <th>Company</th>
-    <th>Contact</th>
-    <th>Country</th>
-  </tr>
-  <tr>
-    <td>Alfreds Futterkiste</td>
-    <td>Maria Anders</td>
-    <td>Germany</td>
-  </tr>
-  <tr>
-    <td>Centro comercial Moctezuma</td>
-    <td>Francisco Chang</td>
-    <td>Mexico</td>
-  </tr>
+  <div class="sample-table">
+    <table>
+      <tr>
+        <th>Company</th>
+        <th>Contact</th>
+        <th>Country</th>
+      </tr>
+      <tr>
+        <td>Alfreds Futterkiste</td>
+        <td>Maria Anders</td>
+        <td>Germany</td>
+      </tr>
+      <tr>
+        <td>Centro comercial Moctezuma</td>
+        <td>Francisco Chang</td>
+        <td>Mexico</td>
+      </tr>
+      </table>
+  </div>
+
 
 </template>
 <script>
